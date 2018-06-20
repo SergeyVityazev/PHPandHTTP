4a14bfe684cdcdc325cfa7a4bba5c6fb58744ea9
SergeyVityazev <37889567+SergeyVityazev@users.noreply.github.com>
2018-06-20 18:39:46 +1000

    Update result.php

--- result.php
+++ result.php
@@ -1,16 +1,15 @@
 <?php
-session_start();
-$i = @imagecreate(200, 200);
+$i = imagecreate(200, 200);
 //Делаем белый фон
 $color = imagecolorallocate($i, 255, 255, 255);
 //Устанавливаем красный цвет
 $text_color = imagecolorallocate($i, 255, 0, 0);
 //Рисуем горизонтальный текст
-imageString($i, 5, 10, 10, "Hello! ".$_SESSION['UserName'], $text_color);
+imageString($i, 5, 10, 10, "Hello! ".$_GET['UserName'], $text_color);
 imageString($i, 5, 10, 80, "Your result:", $color);
 $color = imagecolorallocate($i, 0, 0, 255);
-imagestring($i, 5, 10, 100, "Right answer:".$_SESSION['right'], $text_color);
-imagestring($i, 5, 10, 120, "NoRight answer:".$_SESSION['Noright'], $text_color);
+imagestring($i, 5, 10, 100, "Right answer:".$_GET['RightAnswer'], $text_color);
+imagestring($i, 5, 10, 120, "NoRight answer:".$_POST['NoRightAnswer'], $text_color);
 
 header("Content-type: image/png");
 
@@ -18,3 +17,4 @@ header("Content-type: image/png");
 imagepng($i);
 //Уничтожаем идентификатор и освобождаем ресурсы сервера
 imageDestroy($i);
+
